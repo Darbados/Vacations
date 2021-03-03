061d84152124cc2df347e77a495cc1f6c107c51b
Petar Netev <pesho.netev@gmail.com>
2021-03-03 15:01:57 +0200

    Start reorganizing settings

--- scripts/restoredb.sh
+++ scripts/restoredb.sh
@@ -3,6 +3,6 @@
 set -x -e
 
 sudo whoami
-sudo mysql -u root -e "CREATE USER '`whoami`'; GRANT ALL PRIVILEGES ON *.* TO '`whoami`' WITH GRANT OPTION;"
+#sudo mysql -u root -e "CREATE USER '`whoami`'; GRANT ALL PRIVILEGES ON *.* TO '`whoami`' WITH GRANT OPTION;"
 mysql -e "DROP DATABASE IF EXISTS vacations; CREATE DATABASE vacations;"
-  mysql -e "DROP USER IF EXISTS vacationsuser; CREATE USER vacationsuser IDENTIFIED BY 'dbpassword'; GRANT ALL PRIVILEGES ON vacations.* TO vacationsuser; GRANT ALL PRIVILEGES ON \`test_vacations%\`.* TO vacationsuser;"
+mysql -e "DROP USER IF EXISTS vacationsuser; CREATE USER vacationsuser IDENTIFIED BY 'dbpassword'; GRANT ALL PRIVILEGES ON vacations.* TO vacationsuser; GRANT ALL PRIVILEGES ON \`test_vacations%\`.* TO vacationsuser;"
